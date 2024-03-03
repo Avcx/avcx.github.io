c2372e2ae676ddfe57f93fce60d15fee3abdd690
Tonio Adams <avcx12@gmail.com>
2024-03-03 00:14:23 -0700

    Changed Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import "normalize.css";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <>
-      <Header />
       <main className="page-content">
         <Hero />
-        <Projects />
-        <Contact />
       </main>
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,31 @@ import headshot from "/img/hero.jpg";
 const Hero = () => {
   return (
     <section className="hero">
+      <div className="bg-img" />
       <div className="section-content">
+        <img className="headshot" src={headshot} alt="Photo of Akiel" />
         <div className="hero-card">
           <header className="intro">
             <h1>Akiel Adams</h1>
+            <div className="code-img" />
             <h2>Full-Stack JavaScript Developer</h2>
+
             <h3>Calgary, AB</h3>
           </header>
-          <img src={headshot} alt="Photo of Akiel" />
-        </div>
-        <div className="cta-btn">
-          <a href="#projects">Check Out My Work!</a>
+          <div className="about">
+            <div className="h-text">
+              <p>
+                Hi, I’m Akiel. I’m a JavaScript developer with experience in
+                both front-end and back-end development. I specialize in
+                creating responsive and user-friendly web applications using
+                modern frameworks like Express. I’m also experienced in working
+                with Node.js.
+              </p>
+            </div>
+          </div>
+          <div className="cta-btn">
+            <a href="#projects">My Work</a>
+          </div>
         </div>
       </div>
     </section>
